2430c2f8da2c240a7402c8180d2408b46dffef04
romarioschneider <schneidersoft@ukr.net>
2022-05-30 20:50:58 +0300

    removed vars

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 module "aft" {
   source = "github.com/aws-ia/terraform-aws-control_tower_account_factory?ref=1.4.0"
-  ct_management_account_id    = var.ct_management_account_id
-  log_archive_account_id      = var.log_archive_account_id
-  audit_account_id            = var.audit_account_id
-  aft_management_account_id   = var.aft_management_account_id
-  ct_home_region              = var.ct_home_region
-  tf_backend_secondary_region = var.tf_backend_secondary_region
+  ct_management_account_id    = "596966523393"
+  log_archive_account_id      = "314854817426"
+  audit_account_id            = "435016453613"
+  aft_management_account_id   = "061070186226"
+  ct_home_region              = "eu-central-1"
+  tf_backend_secondary_region = "us-east-1"
 
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
-  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
-  global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
-  account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+  account_request_repo_name                     = "romarioschneider/learn-terraform-aft-account-request"
+  account_provisioning_customizations_repo_name = "romarioschneider/learn-terraform-aft-account-provisioning-customizations"
+  global_customizations_repo_name               = "romarioschneider/learn-terraform-aft-global-customizations"
+  account_customizations_repo_name              = "romarioschneider/learn-terraform-aft-account-customizations"
 
   terraform_distribution = "tfc"
   terraform_token        = var.tfc_token
--- variables.tf
+++ variables.tf
@@ -1,17 +1,3 @@
-variable "ct_management_account_id" {}
-
-variable "log_archive_account_id" {}
-
-variable "audit_account_id" {}
-
-variable "aft_management_account_id" {}
-
-variable "github_username" {}
-
-variable "ct_home_region" {}
-
-variable "tf_backend_secondary_region" {}
-
 variable "tfc_token" {
   description = "The Terraform Cloud Token"
   type        = string
